153e74b3342a83f45f1fa9a4888becccd8b2fd1c
pubuduprabhathiya <pubuduprabhathiya@gmail.com>
2023-03-09 14:31:18 +0530

    Change SYCLKernel, SYCLBody functions

--- cgen/sycl.py
+++ cgen/sycl.py
@@ -63,10 +63,10 @@ def dtype_to_cltype(dtype):
 # {{{ kernel
 
 class SYCLKernel(DeclSpecifier):
-    def __init__(self, subdecl,ndim):
+    def __init__(self, subdecl,ndim,ndrange,queue):
         self.ndim=ndim
-        subdecl.arg_decls.append(Value("sycl::queue","queue_"))
-        subdecl.arg_decls.append(Value("sycl::nd_range<{}> ".format(self.ndim),"range_"))
+        subdecl.arg_decls.append(Value("sycl::queue",queue))
+        subdecl.arg_decls.append(Value("sycl::nd_range<{}> ".format(self.ndim),ndrange))
         DeclSpecifier.__init__(self, subdecl,"")
 
     mapper_method = "map_sycl_kernel"
@@ -166,8 +166,8 @@ class SYCLAccessor(NestedDeclarator):
 
     
 class SYCLBody(Generable):
-    def __init__(self, body,ndim,handler,nd_item):
-        self.body =SYCLQueueSubmit(Block([SYCLparallel_for(body,ndim,handler,nd_item)]),handler)
+    def __init__(self, body,ndim,handler,nd_item,queue,ndrange):
+        self.body =SYCLQueueSubmit(Block([SYCLparallel_for(body,ndim,handler,nd_item,ndrange)]),handler,queue)
 
     def generate(self):
         yield from self.body.generate()
@@ -175,19 +175,20 @@ class SYCLBody(Generable):
     mapper_method = "map_SYCL_function_body"
 
 class SYCLparallel_for(Generable):
-    def __init__(self, body,ndim,handler,nd_item):
+    def __init__(self, body,ndim,handler,nd_item,ndrange):
         self.handler=handler
-        self.args=["range_",FunctionBody(Lamda("=",[Value(f"sycl::nd_item<{ndim}>",nd_item)]),body)]
+        self.args=[ndrange,FunctionBody(Lamda("=",[Value(f"sycl::nd_item<{ndim}>",nd_item)]),body)]
 
     def generate(self):
         yield "{}.parallel_for({});".format(self.handler,", ".join(str(ad) for ad in self.args))
 
     mapper_method = "map_SYCL_parallel_for"
 class SYCLQueueSubmit(Generable):
-    def __init__(self, body,handler):
+    def __init__(self, body,handler,queue):
+        self.queue=queue
         self.args=[FunctionBody(Lamda("&",[Value("sycl::handler &", handler)]),body)]
 
     def generate(self):
-        yield "queue_.submit({}).wait();".format(", ".join(str(ad) for ad in self.args))
+        yield "{}.submit({}).wait();".format(self.queue,", ".join(str(ad) for ad in self.args))
 
     mapper_method = "map_SYCL_queue_submit"
